be654c54236e131c2eb09ceef97ccca0a0edecd6
danyel <berman.danyel@gmail.com>
2024-06-05 09:30:12 +0700

    open linktree

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                     <div class="d-flex align-items-center">
                         
                         <div class="ps-3">
-                            {{-- <span class="text-muted small pt-2 ps-1">https://mylink.kliktes.com/u/{{auth()->user()->referral_code}}</span> --}}
+                            <span class="text-muted small pt-2 ps-1">https://mylink.kliktes.com/u/{{auth()->user()->referral_code}}</span>
                             <ul>
                               <li class="text-muted small pt-2 ps-1">https://portal-cpns.jagotes.id/register?code={{auth()->user()->referral_code}}</li>
                               <li class="text-muted small pt-2 ps-1">https://portal-pppk.jagotes.id/register?code={{auth()->user()->referral_code}}</li>
