c855f8f71ecc64b8213d5627bb62b6dc2f1f9cbc
danyel <berman.danyel@gmail.com>
2025-03-16 16:35:07 +0700

    add html cast

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Casts\HtmlEntity;
 
 class Course extends Model
 {
@@ -11,5 +12,6 @@ class Course extends Model
     
     protected $casts = [
         'info' => 'array',
+        'description' => HtmlEntity::class,
     ];
 }
