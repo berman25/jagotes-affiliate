1cc628c058b6768523b5852d018a021d3d64cc58
danyel <berman.danyel@gmail.com>
2025-10-27 18:41:33 +0700

    ketinggalan

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -182,8 +182,9 @@ class DataController extends Controller
                 $join->on('free_tryout.id', '=', 'tryout_id');
             })
             ->join('users', 'users.user_id', 'jagotes_tryout_participants.user_id')
-            ->whereNotNull('first_attempt_id')
-            ->selectRaw('free_tryout.*, tenant_organization, count(0) as jlh_participant, 
+            ->selectRaw('free_tryout.*, tenant_organization, 
+                        count(first_attempt_score) as jlh_submit, 
+                        count(0) as jlh_register, 
                         max(first_attempt_score) as max_score, 
                         avg(first_attempt_score) as avg_score, 
                         min(first_attempt_score) as min_score')
